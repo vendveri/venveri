2ef390fa9ab3efbdd1e81501d351005fe179e3c6
Kingluddite <howley.phil@gmail.com>
2020-11-15 16:09:41 -0500

    Add gatsby remark package and graphql query for markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,21 @@ Decrease property and employee liabilities, theft, and health risk
     },
     // `gatsby-plugin-eslint`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
