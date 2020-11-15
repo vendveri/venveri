d6c5c37d49d9ccfed9de42106c5911271c72d2ea
Kingluddite <howley.phil@gmail.com>
2020-11-15 16:11:46 -0500

    Move styled-components to their own directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,5 @@
-import React from 'react';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
-import placeholder from '../assets/images/placeholders/image-placeholder.png';
 
-const StyledContentBlock = styled.section`
-  background: ${({background}) => {
-    return background ? `url("${background}")` : 'var(--clr-concrete-white)';
-  }};
-  background-size: cover;
-  color: ${({color}) => {
-    return color || 'inherit';
-  }};
-  box-shadow: var(--dark-shadow);
-  & > div {
-    display: flex;
-    min-height: ${({baseHeight}) => {
-      return baseHeight ? `${baseHeight}vh` : '50vh';
-    }};
-    padding: 2rem;
-    margin: 0 auto;
-    max-width: var(--max-width);
-    justify-content: center;
-    flex-flow: column;
-  }
-  &:first-of-type {
-    & > div {
-      min-height: ${({baseHeight}) => {
-        return `calc(${baseHeight || '50'}vh - 5rem)`;
-      }};
-    }
-  }
-`;
-
-const StyledHeadingBlock = styled.div`
-  h1, h2 {
-    text-shadow: 1px 1px 4px #000;
-  }
-`;
 export const query = graphql`
   {
     block1: contentfulBlock1 {
