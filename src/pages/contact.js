cf435bf4275105a9f1535695eee7c1091a1de939
philip howley <howley.phil@gmail.com>
2020-11-27 14:42:13 -0500

    Add gatsby navigate function for contact form submission redirection to success page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { navigate } from 'gatsby';
 import SEO from '../components/SEO';
 
 const StyledContactPage = styled.section`
