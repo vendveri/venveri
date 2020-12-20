7be9af517a07b3ebac47968a5a9313b433a0056c
philip howley <howley.phil@gmail.com>
2020-12-20 00:16:27 -0500

    Add working mobile navigation

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const Links = ({ styleClass }) => (
+const Links = ({ styleClass, toggleSidebar }) => (
   <ul className={styleClass}>
     <li>
-      <Link to="/#top" className="page-link">
+      <Link to="/#top" className="page-link" onClick={toggleSidebar}>
         <p id="home-page">Home</p>
       </Link>
     </li>
     <li>
-      <Link to="/#industries" className="page-link">
+      <Link to="/#industries" className="page-link" onClick={toggleSidebar}>
         <p id="industries-page">Industries</p>
       </Link>
     </li>
     <li>
-      <Link to="/contact" className="page-link">
+      <Link to="/contact" className="page-link" onClick={toggleSidebar}>
         <p id="contact-page">Contact Us</p>
       </Link>
     </li>
