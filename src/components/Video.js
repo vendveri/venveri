6d912b887c8c6535cfa4ece2fca390b5f40dc36f
philip howley <howley.phil@gmail.com>
2020-12-24 16:31:52 -0500

    Remove autoplay loop and mute

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import video from '../assets/videos/animation.mp4';
 
 export const Video = () => (
   <RegVideoWrapper>
-    <video controls autoPlay muted loop>
+    <video controls>
       <source src={video} type="video/mp4" />
     </video>
   </RegVideoWrapper>
